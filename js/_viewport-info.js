34a9ce885560533c3770146334fde3f2ee2894b1
Massimo Cassandro <massimo.cassandro@gmail.com>
2020-08-25 16:07:15 +0200

    device info fix

--- js/_viewport-info.js
+++ js/_viewport-info.js
@@ -10,15 +10,27 @@ export default function () {
     body_font_size = parseFloat(window.getComputedStyle(document.body, null)
       .getPropertyValue('font-size')),
     getViewportSize = function () {
-      let body_width = document.body.clientWidth,
+      let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0),
+        vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0),
+        vw_em = (vw / body_font_size).toFixed(2),
+        vh_em = (vh / body_font_size).toFixed(2),
+
+        body_width = document.documentElement.clientWidth,
         body_width_em = (body_width / body_font_size).toFixed(2),
-        body_height = document.body.clientHeight,
+        body_height = document.documentElement.clientHeight,
         body_height_em = (body_height / body_font_size).toFixed(2);
 
       lt_vpinfo.innerHTML = `<div class="heading">Viewport info</div>
+        ${vw}${lt.times}${vh}${lt.unitSpacing}px
+        /
+        ${vw_em}${lt.times}${vh_em}${lt.unitSpacing}em
+        <br><br>
+        <strong>body*:</strong><br>
         ${body_width}${lt.times}${body_height}${lt.unitSpacing}px
+        /
+        ${body_width_em}${lt.times}${body_height_em}${lt.unitSpacing}em
         <br>
-        ${body_width_em}${lt.times}${body_height_em}${lt.unitSpacing}em`;
+        <small><em>(*) Altezza uguale a viewport se body height: 100%</em></small>`;
     };
 
 
